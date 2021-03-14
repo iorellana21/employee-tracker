c98e0d66d968b11e8342436f1b3496fbbb85adca
Ivan Orellana Jr <iorellana21@gmail.com>
2021-03-13 22:34:18 -0500

    create connection and setup inquirer prompt

--- server.js
+++ server.js
@@ -10,9 +10,39 @@ const database = mysql.createConnection({
 });
 
 database.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected to MYSQL as id " + database.threadId);
+    if (err) throw err;
+    console.log('connected as id ' + database.threadId);
+    // connection.end();
+    askQuestions();
 });
+
+function askQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter First Name: ",
+                name: "first_name"
+            },
+            {
+                type: "input",
+                message: "Enter Last Name: ",
+                name: "last_name"
+            },
+            {
+                type: "list",
+                message: "What Department does employee belong to?",
+                choices: ["Administrative", "Developmetn", "Support", "Training"],
+                name: "department"
+            },
+            {
+                type: "list",
+                message: "Enter employee's Role: ",
+                choices: ["Administrator", "Developer", "Support", "Trainer", "Admin Manager", "Dev Manager", "Support Manager", "Training Manager"],
+                name: "role"
+            }
+        ])
+        .then(answers => {
+            console.log(answers);
+        });
+}
